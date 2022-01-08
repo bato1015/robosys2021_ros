140929bd6960994334f31f946279f730c7d51922
Ubuntu <ubuntu@ubuntu.mshome.net>
2022-01-08 10:33:36 +0000

    fin

--- scripts/cal_num.py
+++ scripts/cal_num.py
@@ -5,20 +5,21 @@ import rospy
 from std_msgs.msg import Int32
 from mypkg.msg import money
 
+#pub_topic_msg
 angleCount=2
-
 angle = list(range(0, angleCount))
 angle[1]=[0,1,2,3]
 angle[0]=[0,1,0,1]
 
+#user_number
 def select(ration):
     if(ration==2):
-        number=int(input("0~1?"))
+        number=int(input("blue:1 red:2?"))
     elif(ration==4):
-        number=int(input("0~3?"))
-    return number
+        number=int(input("1~4?:"))
+    return number-1
    
-
+#random_num
 def random_num(ration):
     count=random.randint(0,3)
     if(ration==2):
@@ -26,44 +27,30 @@ def random_num(ration):
     elif(ration==4):
         return angle[1][count]
 
-def judge(twice,salary,ru_num,user_num):
+#pub1_topic_msg.salary
+def judge(ration,salary,ru_num,user_num):
     if ru_num==user_num:
-        return twice*salary
+        return ration*salary
     else:
         return 0
 
-def win_judge(money):
-    if money!=0:
-        str1="hit"
-        return str1
-    else:
-        str1="down"
-        return str1
-
-def string1(usr):
-    pub1=rospy.Publisher('result',money,queue_size=1)
-    msg=money()
-    if usr==0:
-        msg.result="loooose!!!!"
-    else:
-        msg.result="win!win!wni!"
-    msg.salary=usr
-    return msg
 
 if __name__ == '__main__': 
    rospy.init_node('sevice_client')
-   pub=rospy.Publisher('rullette_judge1',Int32,queue_size=1)
+   pub=rospy.Publisher('angle_send',Int32,queue_size=1)
+   pub1=rospy.Publisher('result',money,queue_size=1)
    rate=rospy.Rate(10)
 
+   #user_input and judging
    ration = int(input("2?4?"))
-   salary= int(input("money?"))
+   salary= int(input("How much bet?\n"))
    user_num=select(ration)
    ru_num=random_num(ration)
    money1=judge(ration,salary,ru_num,user_num)
-   print(ration,salary,user_num,ru_num,money1)
-   
 
-   pub1=rospy.Publisher('result',money,queue_size=1)
+   print(ration,salary,user_num,ru_num,money1) #answer
+
+   #pun2_topic_msg.result
    msg=money()
    if money1==0:
          msg.result="loooooser!!!!!"
@@ -71,7 +58,8 @@ if __name__ == '__main__':
          msg.result="win!win!win!"
    msg.salary=money1
 
-   for i in range(1):
+   #forは非同期に対応させるため
+   for i in range(1): 
        pub.publish(ru_num)
        for i in range(3):
            pub1.publish(msg)
--- scripts/roulette.py
+++ scripts/roulette.py
@@ -7,18 +7,21 @@ import time
 import RPi.GPIO as GPIO
 from std_msgs.msg import Int32
 
-
+#PIN_set
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 pin=[4,17,27,22]
-StepCount = 8
-angleCount=2
+for i in range(4):
+    GPIO.setup(pin[i], GPIO.OUT)
+
+
 detly=1/1000
 count=0
 v=-1
+angle=[63,191,318,517,446]
 
-angle=[0,90,180,270]
-
+#half_wave
+StepCount = 8
 Seq = list(range(0, StepCount))
 Seq[0] = [1,0,0,1]
 Seq[1] = [1,0,0,0]
@@ -29,28 +32,28 @@ Seq[5] = [0,0,1,0]
 Seq[6] = [0,0,1,1]
 Seq[7] = [0,0,0,1]
 
-for i in range(4):
-    GPIO.setup(pin[i], GPIO.OUT)
-    
+#steping_moter_set
 def setStep(w1, w2, w3, w4):
     GPIO.output(pin[0], w1)
     GPIO.output(pin[1], w2)
     GPIO.output(pin[2], w3)
     GPIO.output(pin[3], w4)
- 
+
+#move_set
 def forward(delay, steps):
     for i in range(steps):
         for j in range(StepCount):
             setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
             time.sleep(delay)
 
+#main_fun
 def rullet(message1):
     global v
     v= message1.data
     t=angle[v]
-    d=360-t
-    for i in range(3):
-        forward(detly,360+30)
+    d=510-t
+    for i in range(2):
+        forward(detly,510)
     forward(detly,t)
     print("戻るまで待って！")
     time.sleep(4)
@@ -58,16 +61,7 @@ def rullet(message1):
     print(v)
 
 
-
-#pub=rospy.Publisher('rulette_sever1',Int32,queue_size=100)
-#pub.publish(count)
-
-
-
 if __name__ == '__main__':
- # rullet(1)
     rospy.init_node('rulette_sever1')
-    sub=rospy.Subscriber('rullette_judge1',Int32,rullet)
+    sub=rospy.Subscriber('angle_send',Int32,rullet)
     rospy.spin()
-    print("r")
-    
